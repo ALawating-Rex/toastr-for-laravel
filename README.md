08ec929d7eb0b9422abc9873876b4c707cd46fed
aex <ocwhhappy@163.com>
2018-02-02 15:00:19 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+## About Toastr-For-Laravel
+Just a test for laravel package development
+
+## License
+
+The Laravel framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
---
+++ aex/toastr/src/Facades/Toastr.php
@@ -0,0 +1,12 @@
+<?php
+namespace Aex\Toastr\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Toastr extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'toastr';
+    }
+}
